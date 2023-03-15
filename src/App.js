28f054be9981c0dbd75e43c66292dc6a115565e3
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-03-15 11:34:17 +0100

    Sum of teo numbers function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import './App.css';
 
+function add(a,b){
+  let num1 = a;
+  let num2 = b;
+  let sum = num1 + num2;
+  console.log(sum)
+}
+add();
+
+function subtract(a,b){
+  
+}
 function App() {
   return (
     <div className="App">
