f5885e246aabb135b6904a93e635a50fd19cf1f0
Onuh Martha <onuh4martha@gmail.com>
2023-03-17 12:43:16 +0100

    the ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import Layout from './components/ui/Layout';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Layout /> 
 
     </div>
   );
---
+++ src/components/ui/Layout.js
@@ -0,0 +1,44 @@
+import React from "react"
+import './layout.css'
+
+
+const Layout = (props) =>{
+    return(
+        <div>
+                <div className="heading">
+                <h5>calc</h5>
+                <h6>THEME</h6>
+            </div>
+        <div className="layout">
+            <div className="displayScreen">399,981</div>
+            <div className="button-box">
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button>DEL</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>+</button>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>-</button>
+                <button>.</button>
+                <button>0</button>
+                <button>/</button>
+                <button>X</button>
+                
+            </div>
+            <div className="last-btn">
+            <button className="reset-btn">RESET</button>
+            <button className="equal-btn">=</button>
+            </div>
+ 
+        </div>
+        </div>
+    )
+}
+export default Layout;
+
+
