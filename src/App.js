7329d24feb87deb1034dc761b6faf4bc9add7be4
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-03-17 13:31:24 +0100

    deleted functions

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import './App.css';
 
-function add(a,b){
-  let num1 = a;
-  let num2 = b;
-  let sum = num1 + num2;
-  console.log(sum)
-}
-add();
 
-function subtract(a,b){
-  
-}
+
 function App() {
   return (
     <div className="App">
